bc917e985b78b9803ce29bb3da446ba137940078
NipunaMadhushan <nipuna.madhushan96@gmail.com>
2024-07-25 11:40:44 +0530

    Update opentelemetry dependency versions

--- native/src/main/java/module-info.java
+++ native/src/main/java/module-info.java
@@ -19,7 +19,6 @@ module io.ballerina.observe.trace.extension.zipkin {
     requires io.opentelemetry.exporter.zipkin;
     requires io.ballerina.runtime;
     requires io.opentelemetry.api;
-    requires io.opentelemetry.api.metrics;
     requires io.opentelemetry.context;
     requires io.opentelemetry.sdk.trace;
     requires io.opentelemetry.sdk.common;
